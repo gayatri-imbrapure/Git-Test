e70ce09d585f94847e62599ee01d1c189932ad92
gayatri-imbrapure <146913542+gayatri-imbrapure@users.noreply.github.com>
2024-09-11 19:48:01 +0530

    Commiting the new file

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -6,43 +6,6 @@
 // thefore doing start+(end-start)/2 will make sure that the bigger number is subracted and the mid will in the integer range 
 
 
-// #include<iostream>
-// using namespace std;
-// int binarySearch(int arr[],int size,int key){
-//     int start=0;
-//     int end=size-1;
-//     int mid=start+(end-start)/2;
-//     while(start<=end){
-//         if(arr[mid]==key){
-//             return mid;
-
-//         }
-
-//         if(arr[mid]<key){
-//             start=mid+1;
-//         }
-//         else if(arr[mid]>key){
-//             end=mid-1;
-//         }
-
-//         mid=start+(end-start)/2;
-//     }
-//     return -1;
-// }
-// int main(){
-//     int even[6]={12,13,14,15,16,17};
-//     int odd[5]={10,20,30,40,50};
-
-//     int evenIndex=binarySearch(even,6,15);
-//     int oddIndex=binarySearch(odd,5,20);
-
-//     cout<<"Element 15 is present at index "<<evenIndex<<endl;
-//     cout<<"Element 20 is present at index "<<oddIndex<<endl;
-
-//     return 0;
-
-
-// }
 
 
 #include<bits/stdc++.h>
